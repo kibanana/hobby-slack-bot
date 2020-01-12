49bf19f41f0f9974c8aa56486cfc9232cda9192b
Yewon Kim <37951612+kyw017763@users.noreply.github.com>
2020-01-13 02:06:58 +0900

    Fix plural value in scrapeMovieText(), Remove useless comment

--- src/scrapeMovie.ts
+++ src/scrapeMovie.ts
@@ -7,6 +7,11 @@ const scrapeMovieText = async (): Promise<object[]> => {
   await fetch('https://movie.naver.com/movie/running/current.nhn')
     .then((res) => {
       let movieTitle: string;
+      
+      let movieGenre: string[] = [];
+      let movieDirector: string[] = [];
+      let movieActors: string[] = [];  
+      
       const $ = res.$;
 
       $('dl.lst_dsc').each((i, elem) => {
@@ -17,13 +22,30 @@ const scrapeMovieText = async (): Promise<object[]> => {
   
         try {
           movieTitle = $(elem).find('dt.tit a').text();
+          
           if (movieTitle) {
+            movieGenre = [];
+            movieDirector = [];
+            movieActors = [];
+
+            $(elem).find('dd dl.info_txt1 dd:first-of-type span.link_txt a').each((j, jElem) => {
+              movieGenre.push($(jElem).text());
+            });
+
+            $(elem).find('dd dl.info_txt1 dd:nth-of-type(2) span.link_txt a').each((x, xElem) => {
+              movieDirector.push($(xElem).text());
+            });
+
+            $(elem).find('dd dl.info_txt1 dd:last-child span.link_txt a').each((z, zElem) => {
+              movieActors.push($(zElem).text());
+            });
+
             result.push({
               title: movieTitle,
               ticketRate: $(elem).find('dd.star dl.info_exp dd div span.num').text(),
-              genre: $(elem).find('dd dl.info_txt1 dd:first-of-type span.link_txt a').text(),
-              director: $(elem).find('dd dl.info_txt1 dd:nth-of-type(2) span.link_txt a').text(),
-              actors: $(elem).find('dd dl.info_txt1 dd:last-child span.link_txt a').text(),
+              genre: movieGenre,
+              director: movieDirector,
+              actors: movieActors,
             });
           }
         } catch (e) {
@@ -36,39 +58,6 @@ const scrapeMovieText = async (): Promise<object[]> => {
     } else {
       return [{}];
     }
-
-  // fetch('https://movie.naver.com/movie/running/current.nhn', (err, $, res, body) => {
-  //   let movieTitle: string;
-
-  //   $('dl.lst_dsc').each((i, elem) => {
-  //     // always reult.length === 7
-  //     if (i >= 7) {
-  //       return true;
-  //     }
-
-  //     try {
-  //       movieTitle = $(elem).find('dt.tit a').text();
-  //       if (movieTitle) {
-  //         result.push({
-  //           title: movieTitle,
-  //           ticketRate: $(elem).find('dd.star dl.info_exp dd div span.num').text(),
-  //           genre: $(elem).find('dd dl.info_txt1 dd:first-of-type span.link_txt a').text(),
-  //           director: $(elem).find('dd dl.info_txt1 dd:nth-of-type(2) span.link_txt a').text(),
-  //           actors: $(elem).find('dd dl.info_txt1 dd:last-child span.link_txt a').text(),
-  //         });
-  //       }
-  //     } catch (e) {
-  //       return false;
-  //     }
-  //   });
-  // });
-  
-  // if (result.length === 7) {
-  //   return result;
-  // } else {
-  //   return false;
-  // }
-
 };
 
 export { scrapeMovieText };
