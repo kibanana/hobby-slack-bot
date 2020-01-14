52b6ce60c24a2e9e88b22b6fc6cfdd9fd4365203
Yewon Kim <37951612+kyw017763@users.noreply.github.com>
2020-01-15 02:45:24 +0900

    Add scrapeMovieImage() for getting movie information image

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { RTMClient } from '@slack/rtm-api';
 
 import config from './config';
-import { scrapeMovieText } from './scrapeMovie';
+import { scrapeMovieText, scrapeMovieImage } from './scrapeMovie';
 
 const token: string = config.SLACK_BOT_TOKEN;
 const rtm = new RTMClient(token);
@@ -15,13 +15,26 @@ rtm.on('message', async (event) => {
   try {
     await rtm.sendMessage(`Hello <@${event.user}>!`, event.channel);
     if (text.includes('!영화')) {
-      scrapeMovieText().then(async (movieInfo) => {
-        if (movieInfo) {
-          await rtm.sendMessage(movieInfo, event.channel);
-        } else {
-          await rtm.sendMessage(`An error occurred during getting movie information!`, event.channel);
-        }
-      });
+      const imageWordArr = ['이미지', '사진', '스크린샷', 'img', 'image', 'screenshot'];
+      if (imageWordArr.includes(text.split('!영화')[1].trim())) {
+        await scrapeMovieImage().then(async (result) => {
+          if (result) {
+            console.log('Success');
+            // stuff
+          } else {
+            await rtm.sendMessage(`An error occurred during getting movie image!`, event.channel);
+          }
+        });
+      } else { // text가 default
+        scrapeMovieText().then(async (movieInfo) => {
+          if (movieInfo) {
+            await rtm.sendMessage(movieInfo, event.channel);
+          } else {
+            await rtm.sendMessage(`An error occurred during getting movie information!`, event.channel);
+          }
+        });
+      }
+      
     } else if (text.includes('!책') || text.includes('!도서')) {
       
     } else {
--- src/scrapeMovie.ts
+++ src/scrapeMovie.ts
@@ -1,4 +1,5 @@
 import { fetch } from 'cheerio-httpcli';
+import * as puppeteer from 'puppeteer';
 
 const scrapeMovieText = async (): Promise<string> => {
 
@@ -85,4 +86,35 @@ const scrapeMovieText = async (): Promise<string> => {
     }
 };
 
-export { scrapeMovieText };
+const scrapeMovieImage = async () => {
+  let result = true;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://movie.naver.com/movie/running/current.nhn#', { waitUntil: 'networkidle2' });
+  await page.setViewport({
+    width: 1280,
+    height: 1430,
+  });
+
+  const main = await page.$('#content');
+  const mainResult = await main!.boundingBox();
+
+  if (mainResult) {
+    await page.screenshot({
+      path: 'naverMovie.png',
+      clip: {
+        x: mainResult.x,
+        y: mainResult.y,
+        width: Math.min(mainResult.width, page.viewport().width),
+        height: Math.min(mainResult.height, page.viewport().height),
+      }, 
+    });
+  } else {
+    result = false;
+  }
+
+  await browser.close();
+  return result;
+};
+
+export { scrapeMovieText, scrapeMovieImage };
