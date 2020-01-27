31ef848e429c4c306eff5ad192b8f36ce6cff312
Yewon Kim <37951612+kyw017763@users.noreply.github.com>
2020-01-28 05:06:53 +0900

    Ensure scrapeBookText() with url

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,7 @@ interface IselectPayload {
 const signingToken: string = process.env.SIGNING_TOKEN ? process.env.SIGNING_TOKEN : configFile.SIGNING_TOKEN;
 const token: string = process.env.ACCESS_TOKEN ? process.env.ACCESS_TOKEN : configFile.ACCESS_TOKEN;
 const port: number = process.env.PORT ? Number(process.env.PORT) : configFile.PORT;
+const channel: string = process.env.CHANNEL ? process.env.CHANNEL : configFile.CHANNEL;
 const slackWebHookUrl = process.env.SLACK_WEBHOOK_URL ? process.env.SLACK_WEBHOOK_URL : configFile.SLACK_WEBHOOK_URL;
 const actionId:string = 'bookSelect';
 const errorMessage = "An error occurred";
@@ -48,12 +49,24 @@ http.createServer(app).listen(port, () => {
 slackInteractions.action(actionId, async (payload: IselectPayload, respond: any) => {
   const categoryResult: string = payload.actions[0].selected_options['0']['value'];
   const categoryResultIdx: number = categoryNameArr.indexOf(categoryResult);
+  getScrapedBookInfo(categoryArr[categoryResultIdx].url);
   await respond({
     text: `*${categoryArr[categoryResultIdx].v}* 카테고리 선택`,
     response_type: 'in_channel',
     replace_original: true
   });
 });
+
+function getScrapedBookInfo(url: string) {
+  scrapeBookText(url).then(async (bookInfo) => {
+    if (bookInfo) {
+      await rtm.sendMessage(bookInfo, channel);
+    } else {
+      await rtm.sendMessage(`${errorMessage} during getting book information!`, channel);
+      return ;
+    }
+  });
+}
 
 const rtm = new RTMClient(token);
 
@@ -64,40 +77,42 @@ const rtm = new RTMClient(token);
 rtm.on('message', async (event) => {
   const text: string = event.text ? event.text : ' ';
 
+  // event.channel === channel
+
   // image send 후 사용자가 별다른 메시지를 보내지 않아도
   // Event가 발생해서 무한루프가 돌길래 if문으로 체크해서 종료
   if (!((!text.trim()) || text.includes(errorMessage) || text.includes(unrecogErrorMessage) || text.includes('Hello'))) {
     try {
-      // await rtm.sendMessage(`Hello <@${event.user}>!`, event.channel);
+      // await rtm.sendMessage(`Hello <@${event.user}>!`, channel);
       if (text.includes('!영화')) {
         const imageWordArr = ['이미지', '사진', '스크린샷', 'img', 'image', 'screenshot'];
         if (imageWordArr.includes(text.split('!영화')[1].trim())) {
           await scrapeMovieImage().then(async (result) => {
             if (result) {
-              await rtm.sendMessage(`<${result}|Movie Image>`, event.channel);
+              await rtm.sendMessage(`<${result}|Movie Image>`, channel);
             } else {
-              await rtm.sendMessage(`${errorMessage} during getting movie image!`, event.channel);
+              await rtm.sendMessage(`${errorMessage} during getting movie image!`, channel);
               return ;
             }
           });
         } else { // text가 default
           scrapeMovieText().then(async (movieInfo) => {
             if (movieInfo) {
-              await rtm.sendMessage(movieInfo, event.channel);
+              await rtm.sendMessage(movieInfo, channel);
             } else {
-              await rtm.sendMessage(`${errorMessage} during getting movie information!`, event.channel);
+              await rtm.sendMessage(`${errorMessage} during getting movie information!`, channel);
               return ;
             }
           });
         }
       } else if (text.includes('!책') || text.includes('!도서')) { // 책 1단계
   
-        const confirmObj = {
-          "title": "Are you sure?",
-          "text": "Wouldn\'t you prefer other category?",
-          "ok_text": "Yes",
-          "dismiss_text": "No"
-        };
+        // const confirmObj = {
+        //   "title": "Are you sure?",
+        //   "text": "Wouldn\'t you prefer other category?",
+        //   "ok_text": "Yes",
+        //   "dismiss_text": "No"
+        // };
         const selectCategory = {
           "type": "interactive_message",
           // "thread_ts": event.ts, // 사용성 저하 우려
@@ -156,27 +171,17 @@ rtm.on('message', async (event) => {
         .then(res => res.text())
         .then(body => {})
         .catch(async (err) => {
-          await rtm.sendMessage(unrecogErrorMessage, event.channel);
-        });
-
-        return ;
-        scrapeBookText(categoryArr[0].url).then(async (bookInfo) => {
-          if (bookInfo) {
-            await rtm.sendMessage(bookInfo, event.channel);
-          } else {
-            await rtm.sendMessage(`${errorMessage} during getting book information!`, event.channel);
-            return ;
-          }
+          await rtm.sendMessage(unrecogErrorMessage, channel);
         });
       } else {
         if ((!text.trim()) || text.includes(errorMessage) || text.includes(unrecogErrorMessage) || text.includes('Hello')) {
-          await rtm.sendMessage(unrecogErrorMessage, event.channel);
+          await rtm.sendMessage(unrecogErrorMessage, channel);
           return ;
         }
       }
     } catch (error) {
       if ((!text.trim()) || text.includes(errorMessage) || text.includes(unrecogErrorMessage) || text.includes('Hello')) {
-        await rtm.sendMessage(errorMessage, event.channel);
+        await rtm.sendMessage(errorMessage, channel);
         return ;
       }
     }
